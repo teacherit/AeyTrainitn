cb6ad4af8597f86c9125b93d2b78bce5b3aac66d
patty <pattynutty@gmail.com>
2018-03-22 15:51:51 +0700

    22 March 2018 Authen OK

--- app/src/main/java/teacherit/mbac/ac/th/aeytraining/fragment/MainFragment.java
+++ app/src/main/java/teacherit/mbac/ac/th/aeytraining/fragment/MainFragment.java
@@ -3,12 +3,22 @@ package teacherit.mbac.ac.th.aeytraining.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import teacherit.mbac.ac.th.aeytraining.R;
+import teacherit.mbac.ac.th.aeytraining.utility.GetAllUser;
+import teacherit.mbac.ac.th.aeytraining.utility.MyAlert;
+import teacherit.mbac.ac.th.aeytraining.utility.MyConstant;
 
 /**
  * Created by It9 on 20/3/2561.
@@ -23,23 +33,114 @@ public class MainFragment extends Fragment
 
             //Step 3
             //  Register Controller
-               TextView textView = getView().findViewById(R.id.txtRegister);
-                textView.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-//     Step 4                   Replace Fragment
-                        getActivity().getSupportFragmentManager()
-                                .beginTransaction()
-                                .replace(R.id.contentMainFragment, new RegisterFragment())
-                                .addToBackStack(null)
-                                .commit();
-
-                    }
-                });
+            registerController();
+              // Login Controller
+            loginController();
 
 
         }   // Main Method
-//Step 1
+
+        private void loginController() {
+            Button button = getView().findViewById(R.id.btnLogin);
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    EditText userEditText = getView().findViewById(R.id.edtUser);
+                    EditText passwordEditText = getView().findViewById(R.id.edtPassword);
+                    String userString = userEditText.getText().toString().trim();
+                    String passwordString = passwordEditText.getText().toString().trim();
+                    if (userString.isEmpty()||passwordString.isEmpty()) {
+                        // Have space
+                         MyAlert myAlert = new MyAlert(getActivity());
+                         myAlert.myDialog(getString(R.string.have_space),
+                                 getString((R.string.message_have_space)));
+
+                    }
+                    else
+                    {
+                        // No space
+                        try
+                        {
+                            MyConstant myConstant = new MyConstant();
+                            GetAllUser getAllUser = new GetAllUser(getActivity());
+                            getAllUser.execute(myConstant.getUrlGetAllUserString());
+                            String jsonString = getAllUser.get();
+                            Log.d("22March2018","JSON==>" + jsonString);
+                            String[] columnUserStrings = myConstant.getLoginString();
+                            //Number of column at mySQL in userTABLE
+                            String[] loginStrings = new String[columnUserStrings.length];
+                            boolean statusBool = true;
+                            JSONArray jsonArray = new JSONArray(jsonString);
+                            for (int i=0;i<jsonArray.length();i+=1)
+                            {
+                              JSONObject jsonObject = jsonArray.getJSONObject(i);
+                              if(userString.equals(jsonObject.getString(columnUserStrings[2])))
+                              {
+                                  statusBool = false;
+                                  for (int i1=0;i1<columnUserStrings.length;i1+=1)
+                                  {
+                                      loginStrings[i] = jsonObject.getString(columnUserStrings[i1]);
+                                      Log.d("22March2018", "loginString[" + i1 + "] ==>" +loginStrings[i1]);
+
+                                  }
+                              }
+                              if (statusBool)
+                              {
+//                                  User False
+                                  MyAlert myAlert = new MyAlert(getActivity());
+                                  myAlert.myDialog("User False",
+                                "No this user in Mysql");
+                            }
+                              else if(passwordString.equals(loginStrings[3]))
+                             {
+                                 //  Password True
+
+                                 Toast.makeText(getActivity(), "Welcome" + loginStrings[1],
+                                       Toast.LENGTH_SHORT).show();
+
+                              }
+                              else
+                              {
+                                  //Password False
+                                  MyAlert myAlert = new MyAlert(getActivity());
+                                  myAlert.myDialog("Password False",
+                                          "Please try again password false");
+
+
+                              }
+
+                            }
+
+                        }
+                        catch (Exception e)
+                        {
+                            e.printStackTrace();
+                        }
+
+                    }
+
+
+                } //on click
+            });
+        }
+
+        private void registerController() {
+            TextView textView = getView().findViewById(R.id.txtRegister);
+            textView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+//     Step 4                   Replace Fragment
+                    getActivity().getSupportFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.contentMainFragment, new RegisterFragment())
+                            .addToBackStack(null)
+                            .commit();
+
+                }
+            });
+        }
+
+        //Step 1
         @Nullable
         @Override
         public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
--- app/src/main/java/teacherit/mbac/ac/th/aeytraining/utility/MyConstant.java
+++ app/src/main/java/teacherit/mbac/ac/th/aeytraining/utility/MyConstant.java
@@ -6,7 +6,19 @@ package teacherit.mbac.ac.th.aeytraining.utility;
 
 public class MyConstant {
 //    About URL
+    private String urlGetAllUserString = "http://androidthai.in.th/mar/getAllUser.php";
     private String urlPostUserString = "http://androidthai.in.th/mar/postUser.php";
+//    About Array variable name from mysql userTABLE
+    private String[] LoginString = new String[]{"id", "Name", "User", "Password"};
+
+
+    public String[] getLoginString() {
+        return LoginString;
+    }
+
+    public String getUrlGetAllUserString() {
+        return urlGetAllUserString;
+    }
 
     public String getUrlPostUserString() {
         return urlPostUserString;
